4c048161f82b9dbe30873ec1a86dfcdc35b51cdf
Luis Mendoza <luis@mendoza.io>
2015-03-10 17:28:13 -0600

    Landing page almost done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  adjustHomeElements();
+});
+
+$(window).resize(function(){
+  adjustHomeElements();
+});
+
+function adjustHomeElements(){
+  $('#intro>.container').height($('#intro').height() - 120);
+
+  // Obtener diferencia entre elementos exitentes arriba de la flech y espacio vacío
+  // Restamos 20 para mantener la flecha siempre a 20 px del borde inferior
+  var height = ($('#intro>.container').height() - $('.cta').height() - $('.flecha').height()) - 20;
+
+  // Aplicar padding-top con el valor calculado 
+  $('.flecha').css("padding-top", height + 'px');
+}
+
+$(window).scroll(function() {
+  var trigger = $('#how-it-works').offset().top - 70;
+
+  if ($("#main-nav").offset().top >  trigger) {
+    $("#main-nav").addClass("white");
+  } else {
+    $("#main-nav").removeClass("white");
+  }
+});
