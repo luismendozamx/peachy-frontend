b4b311c61f14bcdfe6599a6b7e740284665c2edb
Luis Mendoza <luis@mendoza.io>
2015-05-05 23:28:50 -0500

    finished dashboard

--- app/scripts/page.js
+++ app/scripts/page.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-  
+
+  $('.switch').click( function(){
+    console.log('Click Switch')
+    $('.boton-verde').toggle(0);
+    $('.boton-rojo').toggle(0);
+  });
+
+  $('.cancel-btn').click( function(){
+    console.log('Click Cancel')
+    $('.cancel-confirm').toggle(0);
+  });
+
 });
 
 $(window).resize(function(){
